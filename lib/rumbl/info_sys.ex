9eefdad8b523b337c6f6c90d77cfe23cc567e926
Zia Zek <ziazek@gmail.com>
2017-02-28 17:58:28 +0800

    (auto message)

--- lib/rumbl/info_sys.ex
+++ lib/rumbl/info_sys.ex
@@ -5,11 +5,11 @@ defmodule Rumbl.InfoSys do
     defstruct score: 0, text: nil, url: nil, backend: nil
   end
 
-  def start_link(backend, query, query_ref, owner, limit) do
+  def start_link(backend, query, query_ref, owner, limit) do 
     backend.start_link(query, query_ref, owner, limit)
   end
 
-  def compute(query, opts \\ []) do
+  def compute(query, opts \\ []) do 
     limit = opts[:limit] || 10
     backends = opts[:backends] || @backends
 
@@ -17,7 +17,7 @@ defmodule Rumbl.InfoSys do
     |> Enum.map(&spawn_query(&1, query, limit))
   end
 
-  defp spawn_query(backend, query, limit) do
+  defp spawn_query(backend, query, limit) do 
     query_ref = make_ref()
     opts = [backend, query, query_ref, self(), limit]
     {:ok, pid} = Supervisor.start_child(Rumbl.InfoSys.Supervisor, opts)
--- lib/rumbl/wolfram.exs
+++ lib/rumbl/wolfram.exs
@@ -15,10 +15,9 @@ defmodule Rumbl.InfoSys.Wolfram do
     |> send_results(query_ref, owner)
   end
 
-  def send_results(nil, query_ref, owner) do
+  defp send_results(nil, query_ref, owner) do
     send(owner, {:results, query_ref, []})
   end
-
   defp send_results(answer, query_ref, owner) do
     results = [%Result{backend: "wolfram", score: 95, text: to_string(answer)}]
     send(owner, {:results, query_ref, results})
